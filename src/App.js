5288e65654514933ad718480bbc27fe23803eea2
codergirlshecodes <acacchiola180@gmail.com>
2024-03-17 05:19:59 -0400

    fixed netlify link

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <main>
       <Dictionary defaultKeyword="hello"/>
       </main> 
-      <footer className="App-footer"><small>Coded by Alexa Cacchiola</small> and is open-sourced on <a href="https://github.com/codergirlshecodes/dictionary-project">GitHub </a>and hosted on Netlify.</footer>
+      <footer className="App-footer"><small>Coded by Alexa Cacchiola and is open-sourced on <a href="https://github.com/codergirlshecodes/dictionary-project">GitHub </a>and hosted on<a href="https://imaginative-longma-bb234b.netlify.app/"> Netlify.</a></small></footer>
       </div>
     </div>
   );
