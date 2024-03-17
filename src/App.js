8afe4f3978c780425d554f43d0f6d099ffac7fe9
codergirlshecodes <acacchiola180@gmail.com>
2024-03-17 04:41:21 -0400

    Put links

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <main>
       <Dictionary defaultKeyword="hello"/>
       </main> 
-      <footer className="App-footer"><small>Coded by Alexa Cacchiola</small></footer>
+      <footer className="App-footer"><small>Coded by Alexa Cacchiola</small> and is open-sourced on <a href="https://github.com/codergirlshecodes/dictionary-project">GitHub </a>and hosted on Netlify.</footer>
       </div>
     </div>
   );
